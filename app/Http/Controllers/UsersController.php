9fbc9faaf98e33b3da06b3d1c92ec8d377a5072d
598465994 <598465994@qq.com>
2021-05-12 07:16:54 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -22,9 +23,28 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+
+        // dd($request->avatar);
+
+        //获取提交的所有数据
+        $data = $request->all();
+
+        //如果上传了头像
+        if ($request->avatar) {
+            //获得图片的路径
+            $result = $uploader->save($request->avatar, 'avatar', $user->id);
+            //如果返回图片路径正确
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        //更新用户资料
+        $user->update($data);
+
+        //重定向跳转
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 }
